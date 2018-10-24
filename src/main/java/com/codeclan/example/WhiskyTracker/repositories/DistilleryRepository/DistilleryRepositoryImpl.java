a4b800cb380ecaf01aee6e64c5e34e68567d7c90
Michael Blumenthal <bloomer25@googlemail.com>
2018-10-24 21:03:55 +0100

    add query for distilleriesbyregion

--- src/main/java/com/codeclan/example/WhiskyTracker/repositories/DistilleryRepository/DistilleryRepositoryImpl.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/DistilleryRepository/DistilleryRepositoryImpl.java
@@ -1,10 +1,16 @@
 package com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.swing.text.html.parser.Entity;
+import java.util.List;
 
 public class DistilleryRepositoryImpl implements DistilleryRepositoryCustom {
 
@@ -12,6 +18,17 @@ public class DistilleryRepositoryImpl implements DistilleryRepositoryCustom {
     EntityManager entityManager;
 
 //TODO:get all distilleries for a region
+    @Transactional
+    public List<Distillery> getAllDistilleriesForRegion(String region){
+        List<Distillery> results = null;
+        Session session = entityManager.unwrap(Session.class);
+        Criteria cr = session.createCriteria(Distillery.class);
+        cr.add(Restrictions.eq("distillery.region", region));
+        results = cr.list();
+        return results;
+    }
+
+
 
 
 
@@ -19,5 +36,16 @@ public class DistilleryRepositoryImpl implements DistilleryRepositoryCustom {
 
 
 //TODO:get all whiskies from a specific distillery of a specific age
+
+
+////TODO:get all whisky from a particular region
+//
+//    @Transactional
+//    public List<Whisky> getAllWhiskiesForRegion(String region){
+//        List<Whisky> results = null;
+//        Session session = entityManager.unwrap(Session.class);
+//        Criteria cr = session.createCriteria(Distillery.class);
+//        cr.createAlias("whiskies")
+//    }
 
 }
--- src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepositoryImpl.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
 
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
 import com.codeclan.example.WhiskyTracker.models.Whisky;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
@@ -24,10 +25,8 @@ public class WhiskyRepositoryImpl implements WhiskyRepositoryCustom {
         Session session = entityManager.unwrap(Session.class);
         Criteria cr = session.createCriteria(Whisky.class);
         cr.add(Restrictions.eq("year", year));
-        results =cr.list();
+        results = cr.list();
         return results;
     }
-
-//TODO:get all whisky from a particular region
 
 }
